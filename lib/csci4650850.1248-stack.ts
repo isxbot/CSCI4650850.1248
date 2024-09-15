c7afb1490cdd8a327d8f50ae097e3d15633cf388
Dillon G. Isaacs Tse <dillon.isaacs_tse@aviture.us.com>
2024-09-15 16:06:38 -0700

    chore: Initialize CDK project.

---
+++ lib/csci4650850.1248-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class Csci46508501248Stack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'Csci46508501248Queue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
---
+++ test/csci4650850.1248.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Csci46508501248 from '../lib/csci4650850.1248-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/csci4650850.1248-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Csci46508501248.Csci46508501248Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
