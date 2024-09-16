3a4f817bd9471e0acb4ea79a52744ab01aa5ed7f
Dillon G. Isaacs Tse <dillon.isaacs_tse@aviture.us.com>
2024-09-15 18:03:56 -0700

    chore: Create launch template and ASG.

--- bin/csci4650850.1248.ts
+++ bin/csci4650850.1248.ts
@@ -5,17 +5,8 @@ import { Csci46508501248Stack } from '../lib/csci4650850.1248-stack';
 
 const app = new cdk.App();
 new Csci46508501248Stack(app, 'Csci46508501248Stack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  env: {
+    account: '418295722595',
+    region: 'us-west-1'
+  },
 });
--- lib/csci4650850.1248-stack.ts
+++ lib/csci4650850.1248-stack.ts
@@ -1,16 +1,49 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { AutoScalingGroup } from 'aws-cdk-lib/aws-autoscaling';
+import {
+  AmazonLinuxImage,
+  Instance,
+  InstanceClass,
+  InstanceType,
+  InstanceSize,
+  KeyPair,
+  LaunchTemplate,
+  Peer,
+  Port,
+  SecurityGroup,
+  Vpc } from 'aws-cdk-lib/aws-ec2';
 
 export class Csci46508501248Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = Vpc.fromLookup(this, 'default-vpc', {
+      isDefault: true,
+    });
+    const instanceType = InstanceType.of(InstanceClass.T3, InstanceSize.NANO);
+    const machineImage = new AmazonLinuxImage;
+    const keyPair = new KeyPair(this, 'bastion', {});
+    const securityGroup = new SecurityGroup(this, 'ec2-security-group', {
+      vpc,
+      allowAllOutbound: true,
+      securityGroupName: 'ec2-security-group',
+    });
+    securityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(22), 'SSH');
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'Csci46508501248Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const launchTemplate = new LaunchTemplate(this, 'launch-template', {
+      instanceType,
+      machineImage,
+      keyPair,
+      securityGroup,
+    });
+
+    const asg = new AutoScalingGroup(this, 'asg', {
+      vpc,
+      desiredCapacity: 3,
+      minCapacity: 2,
+      maxCapacity: 3,
+      launchTemplate,
+    });
   }
 }
