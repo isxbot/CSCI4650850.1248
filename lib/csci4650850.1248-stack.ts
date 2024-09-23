e8d2f6bd5872da333ff3802ef3a4a6902c1fcb0b
Dillon G. Isaacs Tse <dillon.isaacs_tse@aviture.us.com>
2024-09-23 06:59:47 -0700

    feat: Add S3 bucket and assets deployment.

--- lib/csci4650850.1248-stack.ts
+++ lib/csci4650850.1248-stack.ts
@@ -1,49 +1,28 @@
-import * as cdk from 'aws-cdk-lib';
+import * as CDK from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { AutoScalingGroup } from 'aws-cdk-lib/aws-autoscaling';
-import {
-  AmazonLinuxImage,
-  Instance,
-  InstanceClass,
-  InstanceType,
-  InstanceSize,
-  KeyPair,
-  LaunchTemplate,
-  Peer,
-  Port,
-  SecurityGroup,
-  Vpc } from 'aws-cdk-lib/aws-ec2';
+import * as S3 from 'aws-cdk-lib/aws-s3';
+import * as S3Deployment from 'aws-cdk-lib/aws-s3-deployment';
 
-export class Csci46508501248Stack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class Csci46508501248Stack extends CDK.Stack {
+  constructor(scope: Construct, id: string, props?: CDK.StackProps) {
     super(scope, id, props);
 
-    const vpc = Vpc.fromLookup(this, 'default-vpc', {
-      isDefault: true,
+    const assetsBucket = new S3.Bucket(this, "assets", {
+      publicReadAccess: true,
+      blockPublicAccess: {
+        blockPublicAcls: false,
+        blockPublicPolicy: false,
+        ignorePublicAcls: false,
+        restrictPublicBuckets: false,
+      },
+      versioned: true,
+      removalPolicy: CDK.RemovalPolicy.DESTROY,
     });
-    const instanceType = InstanceType.of(InstanceClass.T3, InstanceSize.NANO);
-    const machineImage = new AmazonLinuxImage;
-    const keyPair = new KeyPair(this, 'bastion', {});
-    const securityGroup = new SecurityGroup(this, 'ec2-security-group', {
-      vpc,
-      allowAllOutbound: true,
-      securityGroupName: 'ec2-security-group',
-    });
-    securityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(22), 'SSH');
-
-    const launchTemplate = new LaunchTemplate(this, 'launch-template', {
-      instanceType,
-      machineImage,
-      keyPair,
-      securityGroup,
-    });
-
-    const asg = new AutoScalingGroup(this, 'asg', {
-      vpc,
-      desiredCapacity: 3,
-      minCapacity: 2,
-      maxCapacity: 3,
-      launchTemplate,
+    
+    const assetsPath = './assets';
+    new S3Deployment.BucketDeployment(this, "Deployment", {
+      sources: [S3Deployment.Source.asset(assetsPath)],
+      destinationBucket: assetsBucket,
     });
   }
 }
