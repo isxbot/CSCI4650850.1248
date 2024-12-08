4159d644160c92d08208342ad65a1877a021c8ab
Dillon G. Isaacs Tse <dillon.isaacs_tse@aviture.us.com>
2024-12-08 15:51:45 -0800

    Add VPC, ECR, and ECS resources.

--- lib/csci4650850.1248-stack.ts
+++ lib/csci4650850.1248-stack.ts
@@ -1,82 +1,71 @@
 import * as CDK from 'aws-cdk-lib';
-import { AmazonLinuxImage, Instance, InstanceClass, InstanceSize, InstanceType, KeyPair, Peer, Port, SecurityGroup, SubnetType, Vpc } from 'aws-cdk-lib/aws-ec2';
-import { Credentials, DatabaseInstance, DatabaseInstanceEngine, DatabaseSecret, MysqlEngineVersion } from 'aws-cdk-lib/aws-rds';
+import { RemovalPolicy } from 'aws-cdk-lib';
+import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Subnet, SubnetType, Vpc } from 'aws-cdk-lib/aws-ec2';
+import { Repository } from 'aws-cdk-lib/aws-ecr';
+import { Cluster, Compatibility, ContainerImage, FargateService, Protocol, TaskDefinition } from 'aws-cdk-lib/aws-ecs';
 import { Construct } from 'constructs';
 
 export class Csci46508501248Stack extends CDK.Stack {
   constructor(scope: Construct, id: string, props?: CDK.StackProps) {
     super(scope, id, props);
 
-    const vpc = Vpc.fromLookup(this, 'vpc', {
-      isDefault: true,
-      region: 'us-west-1',
-      vpcId: 'vpc-0df91f85549331198',
+    // Networking
+    const vpc = new Vpc(this, 'vpc', {
+      subnetConfiguration: [
+        {
+          cidrMask: 28,
+          name: 'private',
+          subnetType: SubnetType.PRIVATE_WITH_EGRESS,
+        },
+        {
+          cidrMask: 28,
+          name: 'public',
+          subnetType: SubnetType.PUBLIC,
+        },
+      ],
     });
 
-    // Database
-    const databaseSecurityGroup = new SecurityGroup(this, 'database-security-group', {
-      vpc,
-      allowAllOutbound: true,
-      description: 'RDS Security Group',
-      securityGroupName: 'rds'
+    // Container Resources
+    const repository = new Repository(this, 'repository', {
+      repositoryName: 'fgp',
+      emptyOnDelete: true,
+      removalPolicy: RemovalPolicy.DESTROY,
     });
 
-    databaseSecurityGroup.addIngressRule(
-      Peer.ipv4('47.7.49.59/32'),
-      Port.tcp(3306),
-    );
 
-    const instanceIdentifier = 'mysql1'
-    const secretName = `/${id}/rds/creds/${instanceIdentifier}`.toLowerCase()
-    const credentials = new DatabaseSecret(this, 'mysql-rds-credentials', {
-      secretName,
-      username: 'admin',
-    });
+    // TODO: Configure RDS instance
+    // TODO: Configure Prisma Schema to connect to database instance.
 
-    const database = new DatabaseInstance(this, 'mysql-rds-database', {
-      vpc,
-      vpcSubnets: {
-        onePerAz: true,
-        subnetType: SubnetType.PUBLIC,
-      },
-      securityGroups: [ databaseSecurityGroup ],
-      engine: DatabaseInstanceEngine.mysql({
-        version: MysqlEngineVersion.VER_8_0_39,
-      }),
-      port: 3306,
-      instanceIdentifier,
-      instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.MICRO),
-      credentials: Credentials.fromSecret(credentials),
-    });
 
-    // EC2
-    const instanceSecurityGroup = new SecurityGroup(this, 'ec2-security-group', {
-      vpc,
-      allowAllOutbound: true,
-      description: 'EC2 security group',
-      securityGroupName: 'ec2',
-    });
-    instanceSecurityGroup.addIngressRule(
-      Peer.ipv4('47.7.49.59/32'),
-      Port.tcp(22),
-    );
-    instanceSecurityGroup.addIngressRule(
-      Peer.anyIpv4(),
-      Port.tcp(3000),
-    );
+    // const taskDefinition = new TaskDefinition(this, 'task', {
+    //   compatibility: Compatibility.FARGATE,
+    //   cpu: '1024',
+    //   memoryMiB: '2048',
+    // });
 
-    const instanceType = InstanceType.of(InstanceClass.T3, InstanceSize.MICRO);
-    const machineImage = new AmazonLinuxImage;
-    const keyPair = new KeyPair(this, 'bastion', {});
+    // const container = taskDefinition.addContainer('fgp', {
+    //   image: ContainerImage.fromEcrRepository(repository),
+    // });
 
-    const instance = new Instance(this, 'ec2-instance', {
-      vpc,
-      instanceType,
-      keyPair,
-      machineImage,
-      securityGroup: instanceSecurityGroup,
-    });
+    // container.addPortMappings({
+    //   containerPort: 3000,
+    //   protocol: Protocol.TCP,
+    // });
 
-    database.connections.allowFrom(instance, Port.tcp(3306));
+    // const cluster = new Cluster(this, 'cluster', {
+    //   clusterName: 'fgp',
+    //   vpc,
+    // });
+
+    // const service = new FargateService(this, 'service', {
+    //   cluster,
+    //   taskDefinition,
+    //   vpcSubnets: {
+    //     subnetType: SubnetType.PRIVATE_WITH_EGRESS, 
+    //   },
+    //   serviceName: 'fgp',
+    // });
+  
   }
 }
