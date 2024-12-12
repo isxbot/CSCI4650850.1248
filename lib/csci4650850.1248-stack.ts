42040e1eb56706823d985bad199d7a7e9f94bb23
Dillon G. Isaacs Tse <dillon.isaacs_tse@aviture.us.com>
2024-12-11 18:26:21 -0800

    Add database and load balancer resources.

--- lib/csci4650850.1248-stack.ts
+++ lib/csci4650850.1248-stack.ts
@@ -1,16 +1,17 @@
 import * as CDK from 'aws-cdk-lib';
-import { RemovalPolicy } from 'aws-cdk-lib';
+import { Duration, RemovalPolicy } from 'aws-cdk-lib';
 import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
-import { Subnet, SubnetType, Vpc } from 'aws-cdk-lib/aws-ec2';
+import { InstanceClass, InstanceSize, InstanceType, Peer, Port, SecurityGroup, Subnet, SubnetType, Vpc } from 'aws-cdk-lib/aws-ec2';
 import { Repository } from 'aws-cdk-lib/aws-ecr';
-import { Cluster, Compatibility, ContainerImage, FargateService, Protocol, TaskDefinition } from 'aws-cdk-lib/aws-ecs';
+import { Cluster, Compatibility, ContainerImage, FargateService, ListenerConfig, LogDriver, Protocol, Secret, TaskDefinition } from 'aws-cdk-lib/aws-ecs';
+import { ApplicationLoadBalancer, ApplicationProtocol } from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import { Credentials, DatabaseInstance, DatabaseInstanceEngine, DatabaseSecret, MysqlEngineVersion } from 'aws-cdk-lib/aws-rds';
 import { Construct } from 'constructs';
 
 export class Csci46508501248Stack extends CDK.Stack {
   constructor(scope: Construct, id: string, props?: CDK.StackProps) {
     super(scope, id, props);
 
-    // Networking
     const vpc = new Vpc(this, 'vpc', {
       subnetConfiguration: [
         {
@@ -26,46 +27,120 @@ export class Csci46508501248Stack extends CDK.Stack {
       ],
     });
 
-    // Container Resources
+    // Database
+    const instanceIdentifier = 'mysql';
+    const secretName = `/${id}/rds/creds/${instanceIdentifier}`.toLowerCase();
+    const credentials = new DatabaseSecret(this, 'mysql-credentials', {
+      secretName,
+      username: 'admin',
+    });
+
+    const database = new DatabaseInstance(this, 'mysql-database', {
+      vpc,
+      vpcSubnets: {
+        subnetType: SubnetType.PRIVATE_WITH_EGRESS,
+      },
+      engine: DatabaseInstanceEngine.mysql({
+        version: MysqlEngineVersion.VER_8_0_39,
+      }),
+      port: 3306,
+      instanceIdentifier,
+      instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.MICRO),
+      credentials: Credentials.fromSecret(credentials),
+      databaseName: 'fgp',
+    });
+
+    // Container
     const repository = new Repository(this, 'repository', {
       repositoryName: 'fgp',
       emptyOnDelete: true,
       removalPolicy: RemovalPolicy.DESTROY,
     });
 
+    const taskDefinition = new TaskDefinition(this, 'task', {
+      compatibility: Compatibility.FARGATE,
+      cpu: '1024',
+      memoryMiB: '2048',
+    });
 
-    // TODO: Configure RDS instance
-    // TODO: Configure Prisma Schema to connect to database instance.
+    const container = taskDefinition.addContainer('fgp', {
+      image: ContainerImage.fromEcrRepository(repository),
+      logging: LogDriver.awsLogs({
+        streamPrefix: 'fgp',
+      }),
+      secrets: {
+        DATABASE_USER: Secret.fromSecretsManager(credentials, 'username'),
+        DATABASE_PASSWORD: Secret.fromSecretsManager(credentials, 'password'),
+        DATABASE_HOST: Secret.fromSecretsManager(credentials, 'host'),
+        DATABASE_PORT: Secret.fromSecretsManager(credentials, 'port'),
+      }
+    });
 
+    container.addPortMappings({
+      containerPort: 3000,
+      protocol: Protocol.TCP,
+    });
 
-    // const taskDefinition = new TaskDefinition(this, 'task', {
-    //   compatibility: Compatibility.FARGATE,
-    //   cpu: '1024',
-    //   memoryMiB: '2048',
-    // });
+    const cluster = new Cluster(this, 'cluster', {
+      clusterName: 'fgp',
+      vpc,
+    });
 
-    // const container = taskDefinition.addContainer('fgp', {
-    //   image: ContainerImage.fromEcrRepository(repository),
-    // });
+    // Load Balancer Resources
+    const securityGroup = new SecurityGroup(this, 'security-group', {
+      vpc,
+    });
+    securityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(3000));
 
-    // container.addPortMappings({
-    //   containerPort: 3000,
-    //   protocol: Protocol.TCP,
-    // });
+    const service = new FargateService(this, 'service', {
+      cluster,
+      taskDefinition,
+      vpcSubnets: {
+        subnetType: SubnetType.PRIVATE_WITH_EGRESS, 
+      },
+      serviceName: 'fgp',
+      circuitBreaker: {
+        enable: true,
+        rollback: true,
+      },
+      securityGroups: [
+        securityGroup,
+      ],
+    });
 
-    // const cluster = new Cluster(this, 'cluster', {
-    //   clusterName: 'fgp',
-    //   vpc,
-    // });
+    database.connections.allowFrom(service, Port.tcp(3306));
 
-    // const service = new FargateService(this, 'service', {
-    //   cluster,
-    //   taskDefinition,
-    //   vpcSubnets: {
-    //     subnetType: SubnetType.PRIVATE_WITH_EGRESS, 
-    //   },
-    //   serviceName: 'fgp',
-    // });
+    const loadBalancerSecurityGroup = new SecurityGroup(this, 'load-balancer-security-group', {
+      vpc,
+    });
+    loadBalancerSecurityGroup.addEgressRule(Peer.anyIpv4(), Port.tcp(80));
+
+    const loadBalancer = new ApplicationLoadBalancer(this, 'load-balancer', {
+        vpc,
+        internetFacing: true,
+        securityGroup: loadBalancerSecurityGroup,
+    });
+
+    const httpListener = loadBalancer.addListener('http-listener', {
+        protocol: ApplicationProtocol.HTTP,
+        open: true,
+    });
+
+    service.registerLoadBalancerTargets({
+        containerName: container.containerName,
+        containerPort: 3000,
+        newTargetGroupId: 'fgp',
+        listener: ListenerConfig.applicationListener(httpListener, {
+            protocol: ApplicationProtocol.HTTP,
+            healthCheck: {
+                healthyHttpCodes: '200',
+                interval: Duration.seconds(90),
+                timeout: Duration.seconds(60),
+                path: '/',
+                port: '3000',
+            },
+        }),
+    });
   
   }
 }
